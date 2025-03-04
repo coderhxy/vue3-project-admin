100b2fe9bd1c17fdbbca05951443b7d1e74b15ed
hxy <2538984484@qq.com>
2025-03-04 23:46:03 +0800

    feat(custom): 实现登录界面，引入 element-plus      ，

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import installElementPlus from './plugins/element'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+// 导入全局样式
+import './styles/index.scss'
+
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
