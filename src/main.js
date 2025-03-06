b4039edd2e1b631d9b7a480963bf11588cb32a22
hxy <2538984484@qq.com>
2025-03-07 00:38:17 +0800

    feat(custom): 实现 SvgIcon 组件

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import store from './store'
 import installElementPlus from './plugins/element'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+// 导入 svgIcon
+import installIcons from '@/icons'
+
 // 导入全局样式
 import './styles/index.scss'
 
 const app = createApp(App)
 installElementPlus(app)
+installIcons(app)
 app.use(store).use(router).mount('#app')
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
