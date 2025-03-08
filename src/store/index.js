3c532904fd3c1c0d00c20707014c8de3f4b08fa9
hxy <2538984484@qq.com>
2025-03-08 21:45:35 +0800

    fix(custom): store 中的 getters 和 modules 放错位置引发报错，修复

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import { createStore } from 'vuex'
 import user from './modules/user.js'
 import getters from './getters.js'
 export default createStore({
+  getters,
   modules: {
     user
-  },
-  getters
+  }
 })
