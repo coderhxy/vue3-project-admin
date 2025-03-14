3e6f5d4d337b566be3ab41c7314ee0454dcc57d4
hxy <2538984484@qq.com>
2025-03-14 23:33:11 +0800

    feat(custom): 实现动态 menu

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -2,31 +2,29 @@
   <!-- 一级 menu 菜单 -->
   <el-menu
     :uniqueOpened="true"
-    default-active="2"
+    :default-active="2"
     background-color="#545c64"
     text-color="#fff"
     active-text-color="#ffd04b"
+    router
   >
-    <!-- 子集 menu 菜单 -->
-    <el-sub-menu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项1</el-menu-item>
-      <el-menu-item index="1-2">选项2</el-menu-item>
-    </el-sub-menu>
-    <!-- 具体菜单项 -->
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>导航四</template>
-    </el-menu-item>
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useRouter } from 'vue-router'
+import SidebarItem from './SidebarItem'
+import { filterRouters, generateMenus } from '@/utils/route'
 
 const router = useRouter()
-console.log(router.getRoutes())
+const routes = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateMenus(filterRoutes)
+})
 </script>
