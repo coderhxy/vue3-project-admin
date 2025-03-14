0e6c370a83f35d56c61154c412d657efb14472f7
hxy <2538984484@qq.com>
2025-03-14 23:43:57 +0800

    fix(custom): 修复动态 menu 的属性绑定问题，以及 menu 高亮配色等等

--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- 一级 menu 菜单 -->
   <el-menu
-    :uniqueOpened="true"
-    :default-active="2"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    :unique-opened="true"
+    :default-active="activeMenu"
     router
   >
     <sidebar-item
@@ -18,7 +18,7 @@
 
 <script setup>
 import { computed } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import SidebarItem from './SidebarItem'
 import { filterRouters, generateMenus } from '@/utils/route'
 
@@ -26,5 +26,12 @@ const router = useRouter()
 const routes = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateMenus(filterRoutes)
+})
+
+// 计算高亮 menu 的方法
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
 })
 </script>
